ec442e27778dcc74b1d429682bde1f39b88bba5f
HASAN_RAZA <hraza486767@gmail.com>
2023-05-24 21:23:46 +0500

    updated

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
         <h1>ECOMMERCE</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; HasanRaza</p>
+        <p>Copyrights 2021 &copy; @HasanRaza</p>
       </div>
 
       <div className="rightFooter">
